62c93013668d183774e250e5f977a7335e6ab43e
Boris Okner <boris.okner@gmail.com>
2025-09-21 20:29:15 -0400

    Binary heap (wip)

--- lib/adt/heap.ex
+++ lib/adt/heap.ex
@@ -13,10 +13,13 @@ defmodule InPlace.Heap do
   `opts` - TBD
   """
   def new(capacity, opts \\ []) do
+    opts = Keyword.merge(default_opts(), opts)
     Array.new(capacity + 1)
     |> then(fn ref ->
       Array.put(ref, capacity + 1 , 0)
-      %{capacity: capacity, array: ref, opts: Keyword.merge(default_opts(), opts)}
+      %{capacity: capacity, array: ref, opts: opts,
+        comparator: Keyword.get(opts, :comparator)
+      }
     end)
 
   end
@@ -29,6 +32,10 @@ defmodule InPlace.Heap do
     Array.get(array, size_address(capacity))
   end
 
+  def empty?(heap) do
+    size(heap) == 0
+  end
+
   def inc_size(%{capacity: capacity, array: array} = _heap, delta \\ 1) do
     Array.update(array, size_address(capacity), fn size -> size + delta end)
   end
@@ -37,16 +44,22 @@ defmodule InPlace.Heap do
     at(heap, 1)
   end
 
-  def extract_min(heap) do
-
+  def extract_min(%{array: array} = heap) do
+    case size(heap) do
+      0 -> nil
+      current_size ->
+        Array.swap(array, 1, current_size)
+        inc_size(heap, -1)
+        sift_down(heap, 1)
+    end
   end
 
-  def insert(%{array: array} = heap, key) when is_integer(key) do
-    if size(heap) == 0 do
-      Array.put(array, 1, key)
-    else
-      
-    end
+  def insert(%{capacity: capacity, array: array} = heap, key) when is_integer(key) do
+    current_size = size(heap)
+    if capacity == current_size, do: throw(:heap_over_capacity)
+    new_size = current_size + 1
+    Array.put(array, new_size, key)
+    sift_up(heap, new_size)
     inc_size(heap, 1)
   end
 
@@ -62,12 +75,49 @@ defmodule InPlace.Heap do
     Array.get(array, position)
   end
 
-  defp get_children(heap, parent_position) do
-    {at(heap, 2 * parent_position), at(heap, 2 * parent_position + 1)}
+  defp get_left_child(heap, parent_position) do
+    at(heap, left_child_position(parent_position))
   end
 
-  defp get_parent(heap, child_position) do
-    at(heap, floor(child_position/2))
+  defp get_right_child(heap, parent_position) do
+    at(heap, right_child_position(parent_position))
+  end
+
+  defp left_child_position(parent_position) do
+    2 * parent_position
+  end
+
+  defp right_child_position(parent_position) do
+    2 * parent_position + 1
+  end
+
+  defp get_parent(heap, child_position) when is_integer(child_position) do
+    at(heap, parent_position(child_position))
+  end
+
+  defp parent_position(child_position) when is_integer(child_position) do
+    div(child_position, 2)
+  end
+
+  defp sift_up(%{comparator: compare_fun, array: array} = heap, 1) do
+    :ok
+  end
+
+  defp sift_up(%{comparator: compare_fun, array: array} = heap, position, key \\ nil) do
+    parent = parent_position(position)
+    p_key = at(heap, parent)
+    c_key = key || at(heap, position)
+    if compare_fun.(p_key, c_key) do
+      :ok
+    else
+      Array.put(array, parent, c_key)
+      Array.put(array, position, p_key)
+      sift_up(heap, parent)
+    end
+  end
+
+  defp sift_down(%{comparator: compare_fun, array: array} = heap, position, key \\ nil) do
+    :TODO
   end
 
 end
