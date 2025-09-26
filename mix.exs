4a0fdf9fe209e2e6a3422358072d2850f591ad8f
Boris Okner <boris.okner@gmail.com>
2025-09-25 21:49:02 -0400

    Prepare for hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Inplace.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description()
+
     ]
   end
 
@@ -21,7 +24,26 @@ defmodule Inplace.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:replbug, "~> 1.0.2", only: :dev}
+      {:replbug, "~> 1.0.2", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
+
+    defp description() do
+    "Mutable data structures"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "inplace",
+      # These are the default files included in the package
+      files: ~w(lib src test mix.exs README* LICENSE*
+                ),
+      exclude_patterns: ["misc/**", "scripts/**", "**/*._exs", "**/*._ex"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/bokner/inplace"}
+    ]
+  end
+
 end
