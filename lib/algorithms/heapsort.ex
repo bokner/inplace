d6fedf7d5625a71268e46cb437b6986ec54fcafa
Boris Okner <boris.okner@gmail.com>
2025-09-25 22:03:40 -0400

    heapsort: eliminate reverse

--- lib/algorithms/heapsort.ex
+++ lib/algorithms/heapsort.ex
@@ -3,27 +3,21 @@ defmodule InPlace.HeapSort do
 
   def sort(list, order \\ :asc) do
     size = length(list)
+    sign = (order == :desc && 1 || -1)
     heap = Heap.new(size)
-    Enum.each(list, fn el -> Heap.insert(heap, el) end)
-    extract_all(heap, size)
-    |> then(fn sorted ->
-      if order == :asc do
-        Enum.reverse(sorted)
-      else
-        sorted
-      end
-    end)
+    Enum.each(list, fn el -> Heap.insert(heap, sign * el) end)
+    extract_all(heap, size, sign)
   end
 
-  defp extract_all(heap, size) do
-    extract_next(heap, size, [])
+  defp extract_all(heap, size, sign) do
+    extract_next(heap, size, [], sign)
   end
 
-  defp extract_next(_heap, 0, acc) do
+  defp extract_next(_heap, 0, acc, _sign) do
     acc
   end
 
-  defp extract_next(heap, size, acc) do
-    extract_next(heap, size - 1, [Heap.extract_min(heap)| acc])
+  defp extract_next(heap, size, acc, sign) do
+    extract_next(heap, size - 1, [sign * Heap.extract_min(heap)| acc], sign)
   end
 end
