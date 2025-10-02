40748b3238b8c5523560b7aae562abfcc40e31a4
Boris Okner <boris.okner@gmail.com>
2025-10-01 21:27:32 -0400

    Rewrote unit test for p-queue

--- test/algorithms/priority_queue_test.exs
+++ test/algorithms/priority_queue_test.exs
@@ -1,49 +1,71 @@
 defmodule InPlace.PriorityQueueTest do
   use ExUnit.Case
 
-  alias InPlace.PriorityQueue
+  alias InPlace.PriorityQueue, as: Q
 
   describe "Priority Queue" do
     test "operations" do
       ## Create
-      q = PriorityQueue.new(100)
-      assert PriorityQueue.empty?(q)
-      assert PriorityQueue.size(q) == 0
-      refute PriorityQueue.get_min(q)
-      refute PriorityQueue.extract_min(q)
+      q = Q.new(100)
+      assert Q.empty?(q)
+      assert Q.size(q) == 0
+      refute Q.get_min(q)
+      refute Q.extract_min(q)
 
       ## Insert
-      PriorityQueue.insert(q, :a, 2.5)
-      assert PriorityQueue.size(q) == 1
-      refute PriorityQueue.empty?(q)
-      assert {:a, 2.5} == PriorityQueue.get_min(q)
-      PriorityQueue.insert(q, "b", 2.7)
-      assert {:a, 2.5} == PriorityQueue.get_min(q)
-      PriorityQueue.insert(q, "b", 0)
-      assert {"b", 0} == PriorityQueue.get_min(q)
+      keys =       [10, -2,  -2, 10]
+      priorities = [2.5, 2.7, 0, 0.5]
 
-      ## Extract min
-      assert {"b", 0} == PriorityQueue.extract_min(q)
-      assert {:a, 2.5} = PriorityQueue.extract_min(q)
-      assert {"b", 2.7} == PriorityQueue.extract_min(q)
-      assert PriorityQueue.empty?(q)
-      ## The "backup" mapping
+      # Shuffle and insert into the Q
+      Enum.zip(keys, priorities)
+      |> Enum.shuffle()
+      |> Enum.each(fn {key, priority} ->
+        Q.insert(q, key, priority)
+      end)
+
+      ## We have
+      ## - a key (109) with a 2nd smallest priority
+      ## - 2 values for the same key (-2)
+      ## We expect:
+      ## - the size of priority queue to be 2 after we insert all priority records (to be implemented)
+      ## - the extraction will be in expected order
+      ## - the extraction will only produce priorities one per key
+
+      ## TODO: not implemented yet
+      ## assert Q.size(q) == 2
+
+      sorted = Enum.reduce_while(1..Q.size(q), [], fn _, acc ->
+        p = Q.extract_min(q)
+        p && {:cont, [p | acc]} || {:halt, acc}
+      end)
+
+      assert length(sorted) == 2
+      assert Enum.sort_by(sorted, fn {_key, priority} -> priority end, :desc) == sorted
     end
 
     test "sorting, heapsort-style" do
       priorities =
         Enum.zip(
-        ["abc", "def", :a, :c, {:d, 1}, {:f, 2.5}, 2,   4, 0.25, -12.99],
-        [ 2,     74.2, 1,   8,  22.5,   -3.7,     -0.5, 3.9, 5.1, 120])
-
+        [2, 2,    2, 3, -1, 17,    45,  19,   2,  4,   9,  -12],
+        [2, 74.2, 0, 1,  8, 22.5, -3.7, -0.5, 4,  3.9, 5.1, 120])
         |> Enum.shuffle()
-        q = PriorityQueue.new(length(priorities))
-      Enum.each(priorities, fn {key, priority} -> PriorityQueue.insert(q, key, priority) end)
+
+        q = Q.new(length(priorities))
+      Enum.each(priorities, fn {key, priority} -> Q.insert(q, key, priority) end)
 
       desc_sorted = Enum.reduce(1..length(priorities), [],
-        fn _, acc -> [PriorityQueue.extract_min(q) | acc] end
+        fn _, acc -> [Q.extract_min(q) | acc] end
       )
-      assert Enum.sort_by(priorities, fn {_key, priority} -> priority end, :desc) == desc_sorted
+      ## filter out nil values resulted from duplicate priorities
+      |> Enum.filter(fn x -> x end)
+
+      ## For duplicates in original priorities list,
+      ## we'll leave the ones with lesser priority
+      deduped = Enum.reduce(priorities, Map.new(), fn {key, priority}, acc ->
+        Map.update(acc, key, priority, fn existing -> min(existing, priority) end)
+      end)
+      assert Enum.sort_by(deduped, fn {_key, priority} -> priority end, :desc)
+            == desc_sorted
     end
   end
 end
