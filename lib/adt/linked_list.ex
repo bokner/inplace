ffaeb5f92cd507ac9b1637fa83c41bf76d163647
Boris Okner <boris.okner@gmail.com>
2025-11-08 21:53:47 -0500

    Fixes and unit tests

--- lib/adt/linked_list.ex
+++ lib/adt/linked_list.ex
@@ -1,28 +1,32 @@
 defmodule InPlace.LinkedList do
   @moduledoc """
     Single-linked list.
-    The data entries are stored as integers;
+    The data entries are stored as integers.
     they will be interpreted (as references) by the calling application.
     Note: indices are 1-based.
-  """
+    Options:
+      - mapper_fun (optional, &Function.identity/1 by default): Maps data entries to application data
+    """
   alias InPlace.{Array, Stack}
 
   ## List terminator
   @terminator 0
 
-  def new(size) when is_integer(size) and size > 0 do
+  def new(size, opts \\ []) when is_integer(size) and size > 0 do
+    opts = Keyword.merge(default_opts(), opts)
     %{
       capacity: size,
       next: init_links(size),
       free: init_free(size),
-      refs: :atomics.new(size, signed: true)
+      refs: :atomics.new(size, signed: true),
+      mapper_fun: Keyword.get(opts, :mapper_fun)
     }
   end
 
-  def get(list, idx) when is_integer(idx) do
+  def get(%{mapper_fun: mapper} = list, idx) when is_integer(idx) and idx > 0 do
     case get_pointer(list, idx) do
       nil -> nil
-      pointer -> data(list, pointer)
+      pointer -> mapper.(data(list, pointer))
     end
   end
 
@@ -48,16 +52,24 @@ defmodule InPlace.LinkedList do
     allocated = allocate(list)
     Array.put(refs, allocated, data)
     ## Have 'head' point to the new element
-    old_head = get_and_update_head(list, allocated)
+    old_head = get_and_update_head(list, fn _ -> allocated end)
 
     ## Update pointer to next element with current head
     Array.put(next, allocated, old_head)
   end
 
-  def add(%{next: pointers, refs: refs} = list, idx, data) when is_integer(data) do
+  def add(%{next: pointers, refs: refs} = list, idx, data)
+      when is_integer(data) and is_integer(idx) do
     cond do
-      idx > size(list) -> {:error, {:no_index, idx}}
-      empty?(list) -> add_first(list, data)
+      idx == 0 ->
+        add_first(list, data)
+
+      idx > size(list) ->
+        {:error, {:no_index, idx}}
+
+      empty?(list) ->
+        add_first(list, data)
+
       true ->
         idx_pointer = get_pointer(list, idx)
         allocated = allocate(list)
@@ -71,7 +83,18 @@ defmodule InPlace.LinkedList do
     add(list, size(list), data)
   end
 
-  def delete(list, ref) do
+  def delete(%{next: pointers} = list, idx) when is_integer(idx) and idx > 0 do
+    cond do
+      idx > size(list) ->
+        {:error, {:no_index, idx}}
+      idx == 1 -> # Removing head
+        deallocate(list, idx)
+        get_and_update_head(list, fn head -> next(list, head) end)
+      true ->
+        pointer = get_pointer(list, idx - 1)
+        deallocate(list, idx)
+        Array.put(pointers, pointer, next(list, next(list, pointer)))
+    end
   end
 
   def to_list(list) do
@@ -82,8 +105,8 @@ defmodule InPlace.LinkedList do
     Enum.reverse(acc)
   end
 
-  defp to_list(list, pointer, acc) do
-    to_list(list, next(list, pointer), [data(list, pointer) | acc])
+  defp to_list(%{mapper_fun: mapper} = list, pointer, acc) do
+    to_list(list, next(list, pointer), [mapper.(data(list, pointer)) | acc])
   end
 
   def reduce(array, initial_value, reducer \\ fn el, acc -> [el | acc] end)
@@ -99,6 +122,12 @@ defmodule InPlace.LinkedList do
 
   def size(%{capacity: capacity, free: free} = _list) do
     capacity - Stack.size(free)
+  end
+
+  def default_opts() do
+    [
+      mapper_fun: &Function.identity/1
+    ]
   end
 
   ## Implementation
@@ -123,9 +152,9 @@ defmodule InPlace.LinkedList do
     Array.get(next, capacity + 1)
   end
 
-  defp get_and_update_head(%{capacity: capacity, next: next} = list, pointer) do
+  defp get_and_update_head(%{capacity: capacity, next: next} = list, update_fun) do
     current_head = head(list)
-    Array.put(next, capacity + 1, pointer)
+    Array.put(next, capacity + 1, update_fun.(current_head))
     current_head
   end
 
@@ -139,5 +168,9 @@ defmodule InPlace.LinkedList do
 
   defp allocate(%{free: free} = _list) do
     Stack.pop(free) || throw(:list_over_capacity)
+  end
+
+  defp deallocate(%{free: free} = _list, idx) when is_integer(idx) and idx > 0 do
+    Stack.push(free, idx)
   end
 end
