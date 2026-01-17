837b25ce20de199f719a179096054c6b2e977be1
Boris Okner <boris.okner@gmail.com>
2026-01-16 21:05:30 -0500

    BitSet (wip)

---
+++ lib/adt/bitset.ex
@@ -0,0 +1,57 @@
+defmodule InPlace.BitSet do
+  def new(enumerable, capacity \\ nil) do
+
+  end
+
+  def to_list(set) do
+
+  end
+
+  def size(set) do
+  end
+
+  def put(set, element) when is_integer(element) do
+  end
+
+  def delete(set, element) when is_integer(element) do
+  end
+
+  def member?(set, element) when is_integer(element) do
+
+  end
+
+  def disjoint?(set1, set2) do
+  end
+
+  def intersection(set1, set2) do
+  end
+
+  def union(set1, set2) do
+  end
+
+  def subset?(set1, set2) do
+  end
+
+  def equal?(set1, set2) do
+  end
+
+  def first(set) do
+
+  end
+
+  def last(set) do
+  
+  end
+
+  def next(set, element) do
+  end
+
+  def previous(set, element) do
+  end
+
+  def reduce(set, acc, reducer) when is_function(reducer, 2) do
+  end
+
+  def filter(set, filter_fun) when is_function(filter_fun, 1) do
+  end
+end
