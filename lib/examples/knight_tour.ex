c542e7aa6f2d035421c33183afd04bb1f639510b
Boris Okner <boris.okner@gmail.com>
2026-05-25 19:15:35 -0400

    Wip still

--- lib/examples/knight_tour.ex
+++ lib/examples/knight_tour.ex
@@ -1,17 +1,18 @@
 defmodule InPlace.Examples.KnightTour do
-  alias InPlace.{Stack, BitSet, SparseSet}
+  alias InPlace.{Stack, BitSet, LinkedList}
 
   def solve(dimension) when is_integer(dimension) do
-    initial_state = %{moves: _move_stack} = init_state(dimension)
-    solve_impl(initial_state)
+    dimension
+    |> init_state()
+    |> solve_impl()
   end
 
-  defp solve_impl(%{moves: move_stack, scheduled: scheduled} = state) do
-    if Stack.empty?(move_stack) do
-      :no_solution
-    else
-      last_move = move(move_stack)
-
+  defp solve_impl(state) do
+    case Stack.pop(state.moves) do
+      nil ->
+        :no_solution
+    last_move ->
+      add_to_visited(state.visited, last_move)
       if solved?(last_move, state) do
         {:solved, to_solution(last_move, state)}
       else
@@ -24,16 +25,17 @@ defmodule InPlace.Examples.KnightTour do
   defp init_state(dimension) do
     num_fields = dimension * dimension
     moves_stack = Stack.new(num_fields * num_fields)
-    scheduled = BitSet.new(0, num_fields - 1)
-    path = SparseSet.new(num_fields)
+    visited = BitSet.new(0, num_fields - 1)
+    path = LinkedList.new(num_fields)
     %{moves: moves_stack,
-      scheduled: scheduled,
+      visited: visited,
+      path: path,
       neighbours: build_neighbours(dimension),
       dimension: dimension
     }
     |> tap(fn initial_state ->
       initial_position = 0 #Enum.random(0..num_fields-1)
-      add_to_path(path, initial_position)
+
       schedule_move(initial_position, initial_state)
     end)
   end
@@ -65,58 +67,78 @@ defmodule InPlace.Examples.KnightTour do
     end)
   end
 
-  defp add_to_scheduled(scheduled, value) do
-    BitSet.put(scheduled, value)
+
+  defp add_to_visited(visited, value) do
+    IO.inspect(value, label: :add_to_visited)
+    BitSet.put(visited, value)
   end
 
-  defp add_to_path(path, value) do
-    SparseSet.delete(path, value)
+  defp remove_from_visited(visited, value) do
+    IO.inspect(value, label: :remove_from_visited)
+    BitSet.delete(visited, value)
   end
 
-  defp backtrack(path, value) do
-    undeleted = SparseSet.undelete(path)
+
+  defp visited?(visited, value) do
+    BitSet.member?(visited, value)
   end
 
-  defp move(moves_stack) do
-    Stack.pop(moves_stack)
+  defp pop_move(%{moves: moves_stack, path: _path, visited: visited} = state) do
+    case Stack.pop(moves_stack) do
+      nil -> nil
+      move ->
+        if visited?(visited, move) do
+          remove_from_visited(visited, move)
+          pop_move(state)
+        else
+          add_to_visited(visited, move)
+          move
+        end
+    end
   end
 
-  defp schedule_move(move,  %{scheduled: scheduled, moves: moves_stack} = _state) do
-    add_to_scheduled(scheduled, move)
+  defp schedule_move(move,  %{moves: moves_stack} = _state) do
     Stack.push(moves_stack, move)
   end
 
-  defp solved?(last_move, %{scheduled: scheduled, dimension: dim} = _state) do
-    IO.inspect(last_move, label: :move)
-    (BitSet.size(scheduled) == dim * dim - 1) &&
-    !BitSet.member?(scheduled, last_move)
+  defp solved?(last_move, %{dimension: dim, visited: visited} = _state) do
+    IO.inspect(%{last_move: last_move, visited: BitSet.size(visited)}, label: :move)
+    BitSet.size(visited) == dim * dim - 1
   end
 
   defp to_solution(last_move, _state) do
     last_move
   end
 
-  defp process_choices(last_move, %{moves: moves, path: path, scheduled: scheduled, neighbours: neighbours} = state) do
+  defp process_choices(last_move, %{moves: moves, visited: visited, neighbours: neighbours} = state) do
     next_move_positions = Map.get(neighbours, last_move)
     ## Push the neighboring positions that have not been already scheduled to stack
     ## If there is no choice, remove last move from scheduled
     has_neighbours? = Enum.reduce(next_move_positions, false, fn pos, has_neighbours_acc? ->
-      if !scheduled?(scheduled, pos) do
-        schedule_move(pos, state)
-        true
-      else
-        has_neighbours_acc?
-      end
+      cond do
+        visited?(visited, pos) ->
+          has_neighbours_acc? || false
+
+        true ->
+          schedule_move(pos, state)
+          true
+        # true ->
+        #   true
+        end
     end)
 
     if has_neighbours? do
-      add_to_path(path, last_move)
+      #add_to_visited(visited, last_move)
     else
-      IO.inspect(Stack.to_list(moves), label: :backtrack)
+      IO.inspect(%{move: last_move, visited: BitSet.to_list(visited), stack: Stack.to_list(moves)}, label: :backtrack)
+      remove_from_visited(visited, last_move)
+      case pop_move(state) do
+        nil -> nil
+        move ->
+          process_choices(move, state)
+        end
     end
   end
 
-  defp scheduled?(scheduled, position) do
-    BitSet.member?(scheduled, position)
-  end
+
 end
